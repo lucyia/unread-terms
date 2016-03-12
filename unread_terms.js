2a9f69db8e2399731c63cb0d0bc8ea79c8e6ad27
lucyia <lucia.koc@gmail.com>
2016-03-12 23:52:35 +0100

    Added files - index and main script

---
+++ unread_terms.js
@@ -0,0 +1,26 @@
+/**
+ * Visualization of Terms of Service. 
+ * 
+ * @author lucyia (ping@lucyia.com)
+ */
+
+'use strict';
+
+(function(window, document, undefined){
+
+	var pages = [
+		{name: 'Facebook', url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/3-128.png', url_service: 'https://www.facebook.com/terms', url_privacy: 'https://www.facebook.com/full_data_use_policy'},
+		{name: 'LinkedIn', url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/9-128.png', url_service: 'https://www.linkedin.com/legal/user-agreement', url_privacy: 'https://www.linkedin.com/legal/privacy-policy?trk=hb_ft_priv'},
+		{name: 'Twitter', url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/4-128.png', url_service: 'https://twitter.com/tos', url_privacy: 'https://twitter.com/privacy'},
+		{name: 'Tumblr', url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/36-128.png', url_service: 'https://www.tumblr.com/policy/en/terms-of-service', url_privacy: 'https://www.tumblr.com/policy/en/privacy'},
+		{name: 'Instagram', url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/5-128.png', url_service: 'https://www.instagram.com/about/legal/terms/', url_privacy: 'https://www.instagram.com/about/legal/privacy/'},
+		{name: 'Google', url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/2-128.png', url_service: 'https://www.google.com/policies/terms/', url_privacy: 'https://www.google.com/policies/privacy/'}
+	];
+
+	function data2vis() {
+			
+	}
+
+	data2vis();
+	
+})(this, document);

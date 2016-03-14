bb3e1fbc68e17f3122f2ddfb5b3a54fad0e977dc
lucyia <lucia.koc@gmail.com>
2016-03-14 23:56:48 +0100

    Imported script for cross-origin content loading
    Disabled cross origin caused many pages to return no content

--- unread_terms.js
+++ unread_terms.js
@@ -8,6 +8,45 @@
 
 (function(window, document, undefined){
 
+	// Accepts a url and a callback function to run
+	// source: http://code.tutsplus.com/tutorials/quick-tip-cross-domain-ajax-request-with-yql-and-jquery--net-10225
+	function requestCrossDomain( site, callback ) {
+		 
+		// If no url was passed, exit.
+		if ( !site ) {
+			alert('No site was passed.');
+			return false;
+		}
+		 
+		// Take the provided url, and add it to a YQL query. Make sure you encode it!
+		var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=xml&callback=?';
+		 
+		// Request that YSQL string, and run a callback function.
+		// Pass a defined function to prevent cache-busting.
+		$.getJSON( yql, cbFunc );
+		 
+		function cbFunc(data) {
+		// If we have something to work with...
+		if ( data.results[0] ) {
+			// Strip out all script tags, for security reasons.
+			// BE VERY CAREFUL. This helps, but we should do more. 
+			data = data.results[0].replace(/<script[^>]*>[\s\S]*?<\/script>/gi, '');
+			 
+			// If the user passed a callback, and it
+			// is a function, call it, and send through the data var.
+			if ( typeof callback === 'function') {
+				callback(data);
+			}
+		}
+		// Else, Maybe we requested a site that doesn't exist, and nothing returned.
+		else throw new Error('Nothing returned from getJSON.');
+		}
+	}
+
+	requestCrossDomain('https://www.instagram.com/about/legal/terms', function(results) {
+		$('body').html(results);
+	});
+
 	/**
 	 * Visualizes passed data within SVG panel 
 	 *
@@ -20,7 +59,7 @@
 		});
 
 		dataTerms.then(
-			function(response){			
+			function(response){
 				var termsPages = response.map(d => Math.round(numberOfPages(d)));
 		}, function(error) {
 				errorMssg(error, true);

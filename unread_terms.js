030aa79f0fba343586849d8c8668d3046750e88f
lucyia <lucia.koc@gmail.com>
2016-03-16 23:29:29 +0100

    Functions, data update

--- unread_terms.js
+++ unread_terms.js
@@ -32,7 +32,7 @@
 			var typePromises = pages.map(d => pagesContent(d['url_'+type]));
 
 			return Promise.all(typePromises).then(function(response){ 
-					return pages = response.map(d => Math.round(numberOfPages(d)));
+					return pages = response.map(d => numberOfPages(d));
 				}, function(error) {
 					errorMssg(error, true);
 				});
@@ -47,7 +47,7 @@
 			function pagesContent(page, table){
 				// when 'xml' is passed as a 'table' var, some pages return the correct output
 				// but 'html' as 'table' var works for most cases, so it set as default
-				var table = table || 'html';
+				var table = 'html';
 				return $.get('http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20'+table+'%20where%20url%3D%27'+encodeURIComponent(page)+'%27&format=xml', 
 					function (response, status) {
 						return response;
@@ -61,7 +61,7 @@
 			 * @return {number} of standard pages (3000 characters per page)
 			 */
 			function numberOfPages(page) {
-				return $(page).text().length/3000;
+				return Math.round($(page).text().length/3000);
 			}
 		}
 
@@ -102,12 +102,41 @@
 	}
 
 	var pages = [
-		{name: 'Facebook', url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/3-128.png', url_terms: 'http://www.facebook.com/terms', url_privacy: 'https://www.facebook.com/full_data_use_policy'},
-		{name: 'LinkedIn', url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/9-128.png', url_terms: 'http://www.linkedin.com/legal/user-agreement', url_privacy: 'https://www.linkedin.com/legal/privacy-policy?trk=hb_ft_priv'},
-		{name: 'Twitter', url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/4-128.png', url_terms: 'http://twitter.com/tos', url_privacy: 'https://twitter.com/privacy'},
-		{name: 'Tumblr', url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/36-128.png', url_terms: 'http://www.tumblr.com/policy/en/terms-of-service', url_privacy: 'https://www.tumblr.com/policy/en/privacy'},
-		{name: 'Instagram', url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/5-128.png', url_terms: 'http://www.instagram.com/about/legal/terms', url_privacy: 'https://www.instagram.com/about/legal/privacy/'},
-		{name: 'Google', url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/2-128.png', url_terms: 'http://www.google.com/policies/terms', url_privacy: 'https://www.google.com/policies/privacy/'}
+		{	name: 'Facebook', 
+			url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/3-128.png', 
+			url_terms: 'https://www.facebook.com/terms', 
+			url_privacy: 'https://www.facebook.com/full_data_use_policy'
+		},
+		{
+			name: 'LinkedIn', 
+			url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/9-128.png', 
+			url_terms: 'https://www.linkedin.com/legal/user-agreement', 
+			url_privacy: 'https://www.linkedin.com/legal/privacy-policy?trk=hb_ft_priv'
+		},
+		{
+			name: 'Twitter', 
+			url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/4-128.png', 
+			url_terms: 'https://twitter.com/tos', 
+			url_privacy: 'https://twitter.com/privacy'
+		},
+		{
+			name: 'Tumblr', 
+			url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/36-128.png', 
+			url_terms: 'https://www.tumblr.com/policy/en/terms-of-service', 
+			url_privacy: 'https://www.tumblr.com/policy/en/privacy'
+		},
+		{
+			name: 'Instagram', 
+			url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/5-128.png', 
+			url_terms: 'https://www.instagram.com/about/legal/terms', 
+			url_privacy: 'https://www.instagram.com/about/legal/privacy/'
+		},
+		{
+			name: 'Google', 
+			url_image: 'https://cdn0.iconfinder.com/data/icons/social-network-9/50/2-128.png', 
+			url_terms: 'https://www.google.com/policies/terms', 
+			url_privacy: 'https://www.google.com/policies/privacy/'
+		}
 	];
 
 	data2vis(pages);
